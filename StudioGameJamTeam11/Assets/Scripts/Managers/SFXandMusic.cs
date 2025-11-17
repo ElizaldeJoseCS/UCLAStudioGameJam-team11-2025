c16c44a96b7b50c10700dee44ce9da11c32f58ff
Jose Elizalde <125120357+ElizaldeJoseCS@users.noreply.github.com>
2025-11-17 13:38:00 -0800

    audio

---
+++ StudioGameJamTeam11/Assets/Scripts/Managers/SFXandMusic.cs
@@ -0,0 +1,69 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+[System.Serializable]
+public struct SFXEntry
+{
+    public string name;             // Name used to call the SFX
+    public AudioClip clip;          // The audio file
+    public float volume;            // Default volume (0–1)
+}
+public class SFXandMusic : MonoBehaviour
+{
+    public static SFXandMusic instance;
+
+    [Header("Audio Sources")]
+    [SerializeField] private AudioSource musicSource;
+    [SerializeField] private AudioSource sfxSourcePrefab;
+
+    [Header("SFX List")]
+    [SerializeField] private List<SFXEntry> sfxList;
+
+    private Dictionary<string, AudioClip> sfxLookup;
+
+    private void Awake()
+    {
+        if (instance == null)
+            instance = this;
+
+        // Build quick-lookup dictionary
+        sfxLookup = new Dictionary<string, AudioClip>();
+        foreach (var entry in sfxList)
+            if (!sfxLookup.ContainsKey(entry.name))
+                sfxLookup.Add(entry.name, entry.clip);
+    }
+
+    // -------------------------
+    //      MUSIC
+    // -------------------------
+    public void PlayMusic(AudioClip clip, float volume = 1f)
+    {
+        musicSource.clip = clip;
+        musicSource.volume = volume;
+        musicSource.Play();
+    }
+
+    public void StopMusic()
+    {
+        musicSource.Stop();
+    }
+
+    // -------------------------
+    //         SFX
+    // -------------------------
+    public void PlaySFX(string name, Vector3 position, float volume = 1f)
+    {
+        if (!sfxLookup.TryGetValue(name, out AudioClip clip))
+        {
+            Debug.LogWarning($"SFX '{name}' not found!");
+            return;
+        }
+
+        AudioSource audioSource = Instantiate(sfxSourcePrefab, position, Quaternion.identity);
+        audioSource.volume = volume;
+        audioSource.clip = clip;
+        audioSource.Play();
+
+        Destroy(audioSource.gameObject, clip.length + 0.1f);
+    }
+}
---
+++ StudioGameJamTeam11/Assets/Scripts/Managers/inputManager.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class inputManager : MonoBehaviour
+{
+    [SerializeField] GameObject blurBG;
+    [SerializeField] GameObject art1;
+    [SerializeField] GameObject HoldAndRelease;
+    [SerializeField] GameObject tapper;
+    [SerializeField] GameObject secondMinigameArt;
+    [SerializeField] GameObject timeGuess;
+
+    //just for the background
+    int numPressed = 0;
+
+    void OnReleaseButton()
+    {
+        // just for the one time background
+        if (numPressed == 0)
+        {
+            blurBG.SetActive(true);
+            art1.SetActive(true);
+            numPressed++;
+        }
+        // next press will bring up the hold and release minigame
+        else if (numPressed == 1)
+        {
+            blurBG.GetComponent<SpriteRenderer>().sortingOrder = 5;
+            HoldAndRelease.SetActive(true);
+            numPressed++;
+        }
+        // if the second minigame art is active, next press begins the second minigame
+        else if (secondMinigameArt.activeInHierarchy)
+        {
+            blurBG.GetComponent<SpriteRenderer>().sortingOrder = 5;
+            timeGuess.SetActive(true);
+        }
+
+            tapper.GetComponent<tapAndHold>().OnReleaseButton();
+    }
+
+    void OnJump()
+    {
+        tapper.GetComponent<tapAndHold>().OnJump();
+    }
+}
