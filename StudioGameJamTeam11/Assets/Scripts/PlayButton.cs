25d5184f1b8fc5a186822170d45d79508e778340
Jose Elizalde <125120357+ElizaldeJoseCS@users.noreply.github.com>
2025-11-17 17:51:01 -0800

    Title Screen and Sound menu

--- StudioGameJamTeam11/Assets/Scripts/PlayButton.cs
+++ StudioGameJamTeam11/Assets/Scripts/PlayButton.cs
@@ -4,13 +4,30 @@ using UnityEngine.InputSystem;
 
 public class PlayButton : MonoBehaviour
 {
+[SerializeField] GameObject soundPanel;
+[SerializeField] GameObject playButton;
+
+void Update()
+{
+    if (Input.GetKeyDown(KeyCode.Escape))
+    {
+        soundPanel.SetActive(!soundPanel.activeSelf);
+    }
+}
+
+
+    void OnJump()
+    {
+        playButton.SetActive(true);
+
+    }
     void OnReleaseButton()
     { 
         LoadNextScene();
     }
-
     public void LoadNextScene()
     {
+
         int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
         SceneManager.LoadScene(currentSceneIndex + 1);
     }
